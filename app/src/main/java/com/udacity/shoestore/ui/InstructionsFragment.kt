5c73f4bfb9e4f47a63e248d4a2c3763c29d4e2e0
bodhisrikar <tsrikar187@gmail.com>
2020-11-27 18:44:44 -0500

    navigating from instructions screen to shoe list screen

--- app/src/main/java/com/udacity/shoestore/ui/InstructionsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/InstructionsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
@@ -24,6 +25,7 @@ class InstructionsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         instructionsFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+        instructionsFragmentBinding.instructionsButton.setOnClickListener(Navigation.createNavigateOnClickListener(InstructionsFragmentDirections.actionInstructionsFragmentToShoeListFragment()))
         return instructionsFragmentBinding.root
     }
 
