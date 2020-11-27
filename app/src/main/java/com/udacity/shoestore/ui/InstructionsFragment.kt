617e9ff36f79a6369b86661befe2637157a2ffc2
bodhisrikar <tsrikar187@gmail.com>
2020-11-27 14:44:24 -0500

    adding instructions screen

---
+++ app/src/main/java/com/udacity/shoestore/ui/InstructionsFragment.kt
@@ -0,0 +1,32 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InstructionsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InstructionsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    }
+
+    companion object {
+
+    }
+}
--- app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -24,6 +25,7 @@ class WelcomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         welcomeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        welcomeFragmentBinding.instructionsButton.setOnClickListener(Navigation.createNavigateOnClickListener(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment()))
         return welcomeFragmentBinding.root
     }
 
