72c756d55f9ea0a89d93453af101b13980085719
bodhisrikar <tsrikar187@gmail.com>
2020-11-27 12:17:58 -0500

    adding login fragment

---
+++ app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    private lateinit var loginFragmentBinding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        loginFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        return loginFragmentBinding.root
+    }
+
+    companion object {
+
+    }
+}
