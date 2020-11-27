30a7cb54144e8124290b29d9de0cd461330b6f20
bodhisrikar <tsrikar187@gmail.com>
2020-11-27 12:30:24 -0500

    adding welcome fragment

---
+++ app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
@@ -0,0 +1,33 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WelcomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WelcomeFragment : Fragment() {
+    private lateinit var welcomeFragmentBinding: FragmentWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        welcomeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        return welcomeFragmentBinding.root
+    }
+
+    companion object {
+
+    }
+}
