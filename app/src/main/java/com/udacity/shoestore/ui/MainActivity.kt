05a952a40d5b10389ee67cd9d1d90bbef4b06267
bodhisrikar <tsrikar187@gmail.com>
2020-11-27 15:14:55 -0500

    adding view model for main activity

--- app/src/main/java/com/udacity/shoestore/ui/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/ui/MainActivity.kt
@@ -1,16 +1,20 @@
-package com.udacity.shoestore
+package com.udacity.shoestore.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ActivityMainBinding
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainActivityBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/udacity/shoestore/ui/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.udacity.shoestore.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class MainViewModel: ViewModel() {
+    private val _shoeList = MutableLiveData<List<Shoe>>()
+    val shoeList: LiveData<List<Shoe>>
+        get() = _shoeList
+}
