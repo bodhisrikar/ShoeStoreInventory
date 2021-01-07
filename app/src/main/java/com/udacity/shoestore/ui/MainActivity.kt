93daa532c123b184f0c897fdf03f44d98f4b4d88
bodhisrikar <tsrikar187@gmail.com>
2021-01-06 21:26:31 -0500

    adding the logout functionality

--- app/src/main/java/com/udacity/shoestore/ui/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/ui/MainActivity.kt
@@ -2,20 +2,41 @@ package com.udacity.shoestore.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainActivityBinding: ActivityMainBinding
-    private lateinit var mainViewModel: MainViewModel
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        navController = this.findNavController(R.id.nav_fragment)
         setSupportActionBar(mainActivityBinding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id != nc.graph.startDestination) {
+                menuInflater.inflate(R.menu.menu, menu)
+            }
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout_btn -> navController.navigate(navController.graph.startDestination)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
